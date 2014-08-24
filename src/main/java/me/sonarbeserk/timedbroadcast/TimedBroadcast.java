190af2239cc278c850a3279cb92796d92f88d35e
SonarBeserk <sonarbeserk@gmail.com>
2014-08-24 01:24:49 -0400

    add basic JavaPlugin

---
+++ src/main/java/me/sonarbeserk/timedbroadcast/TimedBroadcast.java
@@ -0,0 +1,28 @@
+package me.sonarbeserk.timedbroadcast;
+
+import me.sonarbeserk.beserkcore.plugin.JavaPlugin;
+
+public class TimedBroadcast extends JavaPlugin {
+    public void onEnable() {
+        super.onEnable();
+    }
+
+    @Override
+    public boolean shouldSaveData() {
+        return false;
+    }
+
+    @Override
+    public boolean registerPremadeMainCMD() {
+        return true;
+    }
+
+    @Override
+    public String getPermissionPrefix() {
+        return getName().toLowerCase();
+    }
+
+    public void onDisable() {
+        super.onDisable();
+    }
+}
