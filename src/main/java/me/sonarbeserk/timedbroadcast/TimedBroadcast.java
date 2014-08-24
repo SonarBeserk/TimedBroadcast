5690d99d722ec4717dfa9e9826669797a43022cf
SonarBeserk <sonarbeserk@gmail.com>
2014-08-24 01:52:22 -0400

    proper data settings and basic data file

--- src/main/java/me/sonarbeserk/timedbroadcast/TimedBroadcast.java
+++ src/main/java/me/sonarbeserk/timedbroadcast/TimedBroadcast.java
@@ -1,21 +1,24 @@
 package me.sonarbeserk.timedbroadcast;
 
 import me.sonarbeserk.beserkcore.plugin.JavaPlugin;
+import me.sonarbeserk.timedbroadcast.commands.MainCmd;
 
 public class TimedBroadcast extends JavaPlugin {
 
     public void onEnable() {
         super.onEnable();
+
+        getCommand(getName().toLowerCase()).setExecutor(new MainCmd(this));
     }
 
     @Override
     public boolean shouldSaveData() {
-        return false;
+        return true;
     }
 
     @Override
     public boolean registerPremadeMainCMD() {
-        return true;
+        return false;
     }
 
     @Override
